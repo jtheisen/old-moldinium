a649e33e37b8621731782c96af929dc9e6c6b83d
jtheisen <pass-git@zokfotpik.de>
2020-12-18 11:59:31 +0100

    Migration to netstandard

---
+++ Moldinium4/Command.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace IronStone.Moldinium
+{
+    public delegate Boolean DCommandImplementation(Boolean simulate);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="System.Windows.Input.ICommand" />
+    /// <seealso cref="System.IDisposable" />
+    public class Command : ICommand, IDisposable
+    {
+        DCommandImplementation action;
+
+        SerialWatchSubscription subscriptions = null;
+
+        public Command(DCommandImplementation action)
+        {
+            this.action = action;
+        }
+
+        public Command(Action action)
+        {
+            this.action = simulate => { if (simulate) return true; action(); return true; };
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(Object parameter)
+        {
+            return Repository.Instance.EvaluateAndSubscribe("command", ref subscriptions, Evaluate, NotifyChange);
+        }
+
+        public void Execute(Object parameter)
+        {
+            action(false);
+        }
+
+        public void Dispose() => subscriptions.Dispose();
+
+        void NotifyChange() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        Boolean Evaluate() => action(true);
+    }
+}
